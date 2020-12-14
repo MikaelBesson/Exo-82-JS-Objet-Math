c160010039c6f29e4b646fd3a1e8c1fad0d1dd07
BessonMikael <besson.mikael04@gmail.com>
2020-12-14 11:04:03 +0100

    exo 82

---
+++ exo.js
@@ -0,0 +1,36 @@
+
+console.log("valeur 1 = " + Number.parseInt(34.589456));
+
+
+console.log("valeur 2 = " + Number.parseInt(68.412784));
+
+
+console.log("valeur 3 = " + Math.ceil(39.456));
+
+
+console.log("valeur 4 = " + Math.ceil(27.34876541));
+
+
+console.log("valeur 5 = " + parseInt(Math.ceil(5.68)));
+
+
+console.log("valeur 6 = " + parseInt(Math.ceil(5.41)));
+
+
+console.log("valeur 7 = " + Math.trunc(5.41));
+
+
+console.log("valeur 8 = " + Math.trunc(9.568124));
+
+
+console.log("valeur 9 = " +parseInt(Math.min(5, 1, 12, -1, 45)));
+
+console.log("valeur 10 = " +parseInt(Math.min(9, 12, 16, 44, 3)));
+
+console.log("valeur 11 = " +parseInt(Math.max(5, 1, 12, -1, 45)));
+
+console.log("valeur 12 = " +parseInt(Math.max(9, 12, 16, 44, 3)));
+
+console.log("valeur 13 = " +Math.abs(-6));
+
+console.log("valeur 1 = " +Math.abs(-56));
